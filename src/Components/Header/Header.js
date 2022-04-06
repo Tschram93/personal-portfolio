35ea00da7a6964e3daeb6ff76f6d6fbfdabf7909
TS93 <tschram93@yahoo.com>
2022-04-05 22:31:18 -0400

    basic navbar structure created

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,10 @@ import NavBar from '../NavBar/NavBar';
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className='container'>
+            <div className="text-lg">Travis Schram</div>
+            <NavBar />
+        </div>
     )
 }
 
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const NavBar = () => {
-    return (
-        <div>NavBar</div>
-    )
-}
+	return (
+		<nav className=''>
+			<a href='#'className='text-selected-text'>Home</a>
+			<a href='#work'>My Work</a>
+			<a href='#about'>About</a>
+			<a href='#contact'> <button>Contact</button></a>
+		</nav>
+	);
+};
 
 export default NavBar;
