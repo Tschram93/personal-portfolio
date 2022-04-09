03e4e6f3934704266e49d49ee3e93d1a5e122c92
TS93 <tschram93@yahoo.com>
2022-04-09 00:36:56 -0400

    made card component for projects

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,33 +1,108 @@
 import React from 'react';
-import ColorPicking from '../../images/colorpicking.avif'; 
-import DesignImage from '../../images/designimage.avif'; 
-import ECommerce from '../../images/ecommerce.avif'; 
-import MobileTesting from '../../images/mobiletesting.avif'; 
-import ProgressImage from '../../images/progressimage.avif'; 
+import ColorPicking from '../../images/colorpicking.avif';
+import DesignImage from '../../images/designimage.avif';
+import ECommerce from '../../images/ecommerce.avif';
+import MobileTesting from '../../images/mobiletesting.avif';
+import ProgressImage from '../../images/progressimage.avif';
 import TabletDesign from '../../images/tabletdesign.avif';
 import WorkCard from './WorkCard/WorkCard';
 import UIDesign from '../../images/uidesign.avif';
 
 const Work = () => {
-    return (
-        <div className="container mt-64 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
-        <section className="w-full">
-            <h2 id="projects" className="secondary-title">My Projects</h2>
-            <p className="section-paragraph">I’ve had the pleasure of working on multiple projects, designing and implementing both frontend and backend.</p>
+	return (
+		<div className='container mt-64 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full'>
+			<section className='w-full'>
+				<h2 id='projects' className='secondary-title'>
+					My Projects
+				</h2>
+				<p className='section-paragraph'>
+					I’ve had the pleasure of working on multiple projects, designing and
+					implementing both frontend and backend.
+				</p>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
-                <img src={DesignImage} className="w-full bg-nav h-36 lg:h-72 object-cover" alt='progress of application design' />
-                <img src={TabletDesign} className="w-full hidden md:block bg-nav h-36 lg:h-72 object-cover" alt='tablet designing process' />
-                <img src={ColorPicking} className="w-full hidden md:block bg-nav h-36 lg:h-72 object-cover" alt='color designing process' />
-                <img src={ProgressImage} className="w-full hidden md:block bg-nav h-36 lg:h-72 object-cover" alt='progress designing process' />
-                <img src={UIDesign} className="w-full hidden md:block bg-nav h-36 lg:h-72 object-cover" alt='interface designing process' />
-                <img src={MobileTesting} className="w-full hidden md:block bg-nav h-36 lg:h-72 object-cover" alt='mobile designing process' />
-                <img src={ECommerce} className="w-full hidden md:block bg-nav h-36 lg:h-72 object-cover" alt='E-Commerce example'  />
-            </div>
-        </section>
-        <WorkCard />
-    </div>
-    )
+				<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6'>
+					<WorkCard
+						difficultyText={
+							'Description of something that was difficult to accomplish about this.'
+						}
+						github=''
+						livePage=''
+						picture={ColorPicking}
+						role={'This is the role I filled to complete this application.'}
+						solutionText={
+							'I read documentation and did _____ to make this work.'
+						}
+						titleText={'Application Name'}
+					/>
+					<WorkCard
+						difficultyText={
+							'Description of something that was difficult to accomplish about this.'
+						}
+						github=''
+						livePage=''
+						picture={DesignImage}
+						role={'This is the role I filled to complete this application.'}
+						solutionText={
+							'I read documentation and did _____ to make this work.'
+						}
+						titleText={'Application Name'}
+					/>
+					<WorkCard
+						difficultyText={
+							'Description of something that was difficult to accomplish about this.'
+						}
+						github=''
+						livePage=''
+						picture={UIDesign}
+						role={'This is the role I filled to complete this application.'}
+						solutionText={
+							'I read documentation and did _____ to make this work.'
+						}
+						titleText={'Application Name'}
+					/>
+					<WorkCard
+						difficultyText={
+							'Description of something that was difficult to accomplish about this.'
+						}
+						github=''
+						livePage=''
+						picture={ProgressImage}
+						role={'This is the role I filled to complete this application.'}
+						solutionText={
+							'I read documentation and did _____ to make this work.'
+						}
+						titleText={'Application Name'}
+					/>
+					<WorkCard
+						difficultyText={
+							'Description of something that was difficult to accomplish about this.'
+						}
+						github=''
+						livePage=''
+						picture={MobileTesting}
+						role={'This is the role I filled to complete this application.'}
+						solutionText={
+							'I read documentation and did _____ to make this work.'
+						}
+						titleText={'Application Name'}
+					/>
+					<WorkCard
+						difficultyText={
+							'Description of something that was difficult to accomplish about this.'
+						}
+						github=''
+						livePage=''
+						picture={ECommerce}
+						role={'This is the role I filled to complete this application.'}
+						solutionText={
+							'I read documentation and did _____ to make this work.'
+						}
+						titleText={'Application Name'}
+					/>
+				</div>
+			</section>
+		</div>
+	);
 };
 
 export default Work;
--- src/Components/Work/WorkCard/WorkCard.js
+++ src/Components/Work/WorkCard/WorkCard.js
@@ -2,20 +2,47 @@ import React from 'react';
 
 const WorkCard = (props) => {
 	return (
-		<div className='border-solid border-2 border-secondary flex flex-col height-2 width-2'>
-			WorkCard
-			<div className='flex flex-col'>
-				<div className='md:ml-2 ml-3'>
-					<span className='flex justify-start'>Role <strong>:</strong></span>
-                    <p className='md:my-2 my-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, animi?</p>
+		// Whole Card
+		<div className='border border-4 flex flex-col justify-around p-3 rounded-lg border-secondary'>
+			<div className='flex w-full'>
+				{/* Top section & img  */}
+				<img
+					src={props.picture}
+					className='border-2 border-badge md:border-1 w-full bg-nav h-36 lg:h-72 object-cover'
+					alt='progress of application design'
+				/>
+			</div>
+			{/*Live page & repo button links */}
+			<div className='flex justify-around mt-2'>
+				<button className='bg-theme py-1 px-2 rounded-md'>
+					<a href={props.github}>Code</a>
+				</button>
+				<button className='bg-theme py-1 px-2 rounded-md'>
+					<a href={props.livePage}>Application</a>
+				</button>
+			</div>
+			{/* Info section */}
+			<div className='flex flex-col border-t-2 border-nav md:mt2 mt-3'>
+				<span className='text-xl font-bold mt-2'>{props.titleText}</span>
+				<div className='flex flex-wrap md:ml-2 ml-3 mt-2'>
+					<span className='flex justify-center'>
+						<strong className='text-selected-text'>Role:</strong>
+					</span>
+					<p className=' flex items-start md:my-2 my-1'>{props.role}</p>
 				</div>
-				<div className='md:ml-2 ml-3'>
-					<span className='flex justify-start'>Difficulties <strong>:</strong></span>
-                    <p className='md:my-2 my-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, animi?</p>
+				<div className='flex flex-wrap md:ml-2 ml-3'>
+					<span className='flex justify-center'>
+						<strong className='text-selected-text'>Difficulties:</strong>
+					</span>
+					<p className=' flex items-start md:my-2 my-1'>
+						{props.difficultyText}
+					</p>
 				</div>
-				<div className='md:ml-2 ml-3'>
-					<span className='flex justify-start'>Solution <strong>:</strong></span>
-                    <p className='md:my-2 my-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, animi?</p>
+				<div className='flex flex-wrap md:ml-2 ml-3'>
+					<span className='flex justify-center'>
+						<strong className='text-selected-text'>Solution:</strong>
+					</span>
+					<p className=' flex items-start md:my-2 my-1'>{props.solutionText}</p>
 				</div>
 			</div>
 		</div>
